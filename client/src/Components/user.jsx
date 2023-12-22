ac485c55357d1172f6b7767c6d0e2437e8bc258b
Rijul Sharma <rijul228@gmail.com>
2023-12-22 17:36:48 +0530

    commit1

---
+++ client/src/Components/user.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function user() {
+  return (
+    <div>
+        <input></input>
+        <button>random</button>
+        <button>play</button>
+        <img></img>
+    </div>
+  )
+}
+
+export default user
